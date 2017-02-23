859f75320be0fd9e0922091d493fd05674732161
vhCorp <wms.vhcorp@gmail.com>
2017-02-23 15:03:21 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var tick = 0;
 var cCount;
 var eggCount;
@@ -110,63 +109,3 @@ function draw() {
         }
     }
 }
-=======
-var order = 2;
-var ngrams = {};
-var button;
-var nstr = {};
-var beginnings = []
-var avglen = 0;
-function preload() {
-    names = loadStrings('logs.txt');
-    console.log(names)
-}
-
-function setup() {
-    createP("Length of message:")
-    slider = createSlider(8,100)
-    button = createButton("generate");
-    button.mousePressed(markovIt);
-    noCanvas();
-    for (var j = 0; j < names.length; j++) {
-        var nstr = names[j].split(" ");
-        avglen += nstr.length
-        avglen = avglen / 2
-        for (var i = 0; i <= nstr.length - 1; i++) {
-
-            var gram = nstr[i];
-            if (i === 0) {
-                beginnings.push(gram);
-            }
-            //console.log(ngrams[gram])
-            if (ngrams[gram] == undefined) {
-                ngrams[gram] = [];
-            }
-            if (nstr[i + 1] != undefined) {
-                ngrams[gram].push(nstr[i + 1]);
-            }
-
-        }
-    }
-    //console.log(ngrams);
-}
-
-function markovIt() {
-
-    var currentGram = random(beginnings);
-    var result = currentGram;
-    var len = round(slider.value() + avglen)
-    for (var i = 0; i < len ; i++) {
-        var possibilities = ngrams[currentGram];
-
-        var next = random(possibilities);
-        if (!next) {
-            break;
-        }
-        result = result + " " + next;
-        currentGram = next;
-    }
-
-    createP(result);
-}
->>>>>>> origin/master
